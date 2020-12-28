b8da1ef09490a892000cf0eb9a8d466d9435c5cd
Keiji Yamaki <k.yamaki@hiroshimamm.com>
2020-12-28 23:50:00 +0900

    Initial Commit

---
+++ Sample1-DataLink-App2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Sample1-DataLink-App2
+//
+//  Created by keiji yamaki on 2020/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("App2")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sample1-DataLink-App2/Sample1_DataLink_App2App.swift
@@ -0,0 +1,17 @@
+//
+//  Sample1_DataLink_App2App.swift
+//  Sample1-DataLink-App2
+//
+//  Created by keiji yamaki on 2020/12/28.
+//
+
+import SwiftUI
+
+@main
+struct Sample1_DataLink_App2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
